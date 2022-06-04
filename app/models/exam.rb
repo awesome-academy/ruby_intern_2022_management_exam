a77c80f1496d48570e74e9f107cdc9cff6188f21
anhdh-3069 <duong.hoang.anh@sun-asterisk.com>
2022-06-04 13:23:25 +0700

    user create exam

--- app/models/exam.rb
+++ app/models/exam.rb
@@ -8,4 +8,13 @@ class Exam < ApplicationRecord
 
   delegate :name, :duration, to: :subject, prefix: true, allow_nil: true
   delegate :score, to: :result, prefix: true, allow_nil: true
+  after_save :create_exam_question
+
+  private
+
+  def create_exam_question
+    subject.questions.each do |question|
+      exams_questions.create! question_id: question.id
+    end
+  end
 end
