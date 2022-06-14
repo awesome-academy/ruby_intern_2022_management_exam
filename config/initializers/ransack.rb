697dd13216e20fc8c2ec87d1d7ef1d036cbc8584
anhdh-3069 <duong.hoang.anh@sun-asterisk.com>
2022-06-14 15:32:20 +0700

    add gem ransack

--- config/initializers/ransack.rb
+++ config/initializers/ransack.rb
@@ -1,3 +1,8 @@
 Ransack.configure do |config|
   config.search_key = :search
+  config.add_predicate 'date_equals',
+    arel_predicate: 'eq',
+    formatter: proc {|v| v.to_date},
+    validator: proc {|v| v.present?},
+    type: :string
 end
